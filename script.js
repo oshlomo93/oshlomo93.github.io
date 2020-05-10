da9dadbb2020265b94a0e23d6b446ac0fa67d7bb
oshlomo93 <50537947+oshlomo93@users.noreply.github.com>
2020-05-10 17:47:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+	document.getElementById("login").style = "display: inline-block"
+	document.getElementById("welcome").style = "display: inline-block"
+	document.getElementById("actions_title").style = "display: none"
+	document.getElementById("actions").style = "display: none"
+	document.getElementById("new_team").style = "display: none"
+	document.getElementById("new_team_title").style = "display: none"
+	document.getElementById("league_policy").style = "display: none"
+	document.getElementById("league_policy_title").style = "display: none"
+})
+
+function welcome() {
+	document.getElementById("login").style = "display: inline-block"
+	document.getElementById("welcome").style = "display: inline-block"
+	document.getElementById("actions_title").style = "display: none"
+	document.getElementById("actions").style = "display: none"
+	document.getElementById("new_team").style = "display: none"
+	document.getElementById("new_team_title").style = "display: none"
+	document.getElementById("league_policy").style = "display: none"
+	document.getElementById("league_policy_title").style = "display: none"
+}
+function checkValidation() {
+	//TODO
+	alert("Welcome Back!");
+	showActions();
+}
+
+function showActions() {
+	document.getElementById("login").style = "display: none"
+	document.getElementById("welcome").style = "display: none"
+	document.getElementById("actions_title").style = "display: inline-block"
+	document.getElementById("actions").style = "display: inline-block"
+	document.getElementById("new_team").style = "display: none"
+	document.getElementById("new_team_title").style = "display: none"
+	document.getElementById("league_policy").style = "display: none"
+	document.getElementById("league_policy_title").style = "display: none"
+}
+
+function createNewTeam() {
+	document.getElementById("login").style = "display: none"
+	document.getElementById("welcome").style = "display: none"
+	document.getElementById("actions_title").style = "display: none"
+	document.getElementById("actions").style = "display: none"
+	document.getElementById("new_team").style = "display: inline-block"
+	document.getElementById("new_team_title").style = "display: inline-block"
+	document.getElementById("league_policy").style = "display: none"
+	document.getElementById("league_policy_title").style = "display: none"
+}
+
+function teamCreated() {
+	//TODO
+	alert("The Team Created Successfully!");
+	showActions();
+}
+
+function chooseLeaguePolicy() {
+	document.getElementById("login").style = "display: none"
+	document.getElementById("welcome").style = "display: none"
+	document.getElementById("actions_title").style = "display: none"
+	document.getElementById("actions").style = "display: none"
+	document.getElementById("new_team").style = "display: none"
+	document.getElementById("new_team_title").style = "display: none"
+	document.getElementById("league_policy").style = "display: inline-block"
+	document.getElementById("league_policy_title").style = "display: inline-block"
+}
+
+function updatePolicy() {
+	//TODO
+	alert("The Laegue Policy Updated Successfully!");
+	showActions();
+}
+
+function manageGame() {
+	
+}
+
+function logout() {
+	let ans = confirm("Are You Sure You Want To Leave?");
+	if (ans) {
+		welcome();
+	}
+}
